f077c0e1a9ea1f011e57bf6f954bdf91b4ef7e06
Diogo Silva <12896361+diogols@users.noreply.github.com>
2018-04-15 18:54:48 +0100

    added broadcast

---
+++ Broadcast/broadcast.py
@@ -0,0 +1,87 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from random import randint
+
+
+def connected_graph(vertexes):
+    connected = nx.Graph()
+    connected.add_nodes_from(range(vertexes))
+    is_connected = False
+    while not is_connected:
+        u = randint(0, vertexes-1)
+        v = randint(0, vertexes-1)
+        if not connected.has_edge(u, v) and u != v:
+            connected.add_edge(u, v)
+        is_connected = nx.is_connected(connected)
+    return connected
+
+
+def finished(array):
+    for i in array:
+        if i == -1:
+            return False
+    return True
+
+
+def send(graph, u, rounds, r):
+    result = []
+    for node in u:
+        for v in graph[node]:
+            if rounds[v] == -1:
+                rounds[v] = r
+                result.append(v)
+    return result
+
+
+def array_to_matrix(l):
+    result = [[] for _ in range((max(l) + 1))]
+    for i in range(len(l)):
+        result[l[i]].append(i)
+    return result
+
+
+n = int(input("Insert number of nodes: "))
+if n < 1:
+    exit("You must at least insert one node.")
+g = connected_graph(n)
+
+# Tick 0: choose node
+ticks = [-1 for x in range(n)]
+chosen_one = randint(0, n - 1)
+nodes = [chosen_one]
+tick = 0
+ticks[chosen_one] = tick
+while not finished(ticks):
+    tick += 1
+    nodes = send(g, nodes, ticks, tick)
+
+fig = plt.gcf()
+fig.canvas.set_window_title('Broadcast with' + str(n) + ' nodes')
+fig_manager = plt.get_current_fig_manager()
+fig_manager.full_screen_toggle()
+
+l = array_to_matrix(ticks)
+plt.xticks(range(max(ticks)+1))
+if n < 30:
+    plt.yticks(range(n))
+for i in range(len(l)):
+    for j in l[i]:
+        plt.scatter([i], [j], color='blue')
+
+plt.ylabel('Node')
+plt.xlabel('Round')
+plt.title('Broadcast on connected graph')
+plt.show()
+
+fig = plt.gcf()
+fig.canvas.set_window_title('Broadcast with' + str(n) + ' nodes')
+fig_manager = plt.get_current_fig_manager()
+fig_manager.full_screen_toggle()
+r = [len(x) for x in l]
+plt.yticks(r)
+plt.xticks(range(max(ticks)+1))
+plt.plot(range(len(l)), r, color='blue')
+plt.ylabel('Number of Nodes')
+plt.xlabel('Round')
+plt.title('Broadcast on connected graph')
+plt.show()
